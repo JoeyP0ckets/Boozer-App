5c8d2b186d44c888b40b3a26a889fa035b949ee1
github username <github email address>
2020-06-13 10:28:24 -0400

    search by name function added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Search from "./Search"
+
+
 export default class Navbar extends Component {
   render() {
     return(
       <div className="nav-bar">
        <h3>Login Logout MyFavDrinks AddDrink</h3>  
-      <Search />
+      <Search searchDrinkName={this.props.searchDrinkName}/>
       </div>
     )
   }
